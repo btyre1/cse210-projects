f3a3687045312d21c46679e32204043b1b511217
btyre1 <bryc11499@gmail.com>
2025-05-01 21:33:39 -0400

    Finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 200);
+
+        int guess = 0;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("\nWhat is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess == magicNumber)
+            {
+                Console.WriteLine("You guessed it!\n");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else 
+            {
+                Console.WriteLine("Lower");
+            } 
+        } 
+        
     }
 }
